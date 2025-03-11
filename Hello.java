62bb78406b84cb1a82a1a0bfc427c5bf3e3e17f2
João Guilherme <jgaheinemann@minha.fag.edu.br>
2025-03-11 16:00:44 -0300

    Adicionado método 'Hello World' na branch helloworld

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
-    public static void olaMundo() {
-        System.out.println("Olá Mundo");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
 
