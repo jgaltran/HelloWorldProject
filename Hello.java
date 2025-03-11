79e2e841c7b4c949485f12c3a3c1219131f3ab04
João Guilherme <jgaheinemann@minha.fag.edu.br>
2025-03-11 15:35:37 -0300

    Métodos 'Hello World' e 'Olá Mundo' na branch principal

--- Hello.java
+++ Hello.java
@@ -6,8 +6,5 @@ public class Hello {
     public static void olaMundo() {
         System.out.println("Olá Mundo");
     }
-
-    public static void holaQueTal() {
-        System.out.println("Hola que tal");
-    }
 }
+
