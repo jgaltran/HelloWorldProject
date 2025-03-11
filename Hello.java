68bbb277548c0d4aa9a1c7611e3a2276b38c68de
João Guilherme <jgaheinemann@minha.fag.edu.br>
2025-03-11 15:16:51 -0300

    Adicionado método olaMundo

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    public static void olaMundo() {
+        System.out.println("Olá Mundo");
+    }
+}
+
